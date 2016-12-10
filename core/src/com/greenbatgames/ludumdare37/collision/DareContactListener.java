e1140a9ea1ddfdf651d56896388bf73b370e0964
Arne 'S Jegers <arnesjegers@gmail.com>
2016-12-10 20:29:09 +0100

    Expanded Turret class, added TurretAimComponent. Modest expansion to ContactListener.

--- core/src/com/greenbatgames/ludumdare37/collision/DareContactListener.java
+++ core/src/com/greenbatgames/ludumdare37/collision/DareContactListener.java
@@ -4,10 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.greenbatgames.ludumdare37.entity.PhysicsBody;
 import com.greenbatgames.ludumdare37.iface.Threat;
 import com.greenbatgames.ludumdare37.player.Player;
+import com.greenbatgames.ludumdare37.threat.Turret;
 
 /**
  * Created by Quiv on 10-12-2016.
@@ -21,17 +23,26 @@ public class DareContactListener implements ContactListener {
         Object a = contact.getFixtureA().getBody().getUserData();
         Object b = contact.getFixtureB().getBody().getUserData();
 
-        //DEBUG
-        Gdx.app.log("","Colliding");
+        Fixture fixA = contact.getFixtureA();
+        Fixture fixB = contact.getFixtureB();
 
         if(!(a instanceof PhysicsBody) || !(b instanceof  PhysicsBody)){
             return;
         }
 
-        if(a instanceof Player){
-            handlePlayerContact((Player) a, (PhysicsBody) b);
-        } else if (b instanceof Player) {
-            handlePlayerContact((Player) b, (PhysicsBody) a);
+        if(a instanceof Player && b instanceof Threat){
+            //Check if there is physical contact
+            if(!fixA.isSensor() && !fixB.isSensor()){
+                ((Threat) b).touchPlayer((Player) a);
+            } else if(!fixA.isSensor() && fixB.isSensor()){
+                if(b instanceof Turret){
+
+                }
+            }
+        } else if (b instanceof Player && a instanceof Threat) {
+            if(!fixA.isSensor() && !fixB.isSensor()){
+                ((Threat) a).touchPlayer((Player) b);
+            }
         }
     }
 
@@ -48,11 +59,5 @@ public class DareContactListener implements ContactListener {
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
 
-    }
-
-    private void handlePlayerContact(Player player, PhysicsBody body){
-        if(body instanceof Threat){
-            ((Threat) body).touchPlayer(player);
-        }
     }
 }
--- core/src/com/greenbatgames/ludumdare37/threat/Turret.java
+++ core/src/com/greenbatgames/ludumdare37/threat/Turret.java
@@ -1,5 +1,6 @@
 package com.greenbatgames.ludumdare37.threat;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.greenbatgames.ludumdare37.entity.PhysicsBody;
 import com.greenbatgames.ludumdare37.iface.Threat;
@@ -11,9 +12,12 @@ import com.greenbatgames.ludumdare37.player.Player;
 
 // TODO: Turrets patrol back and forth with a sensor cone, looking for the player
 public class Turret extends PhysicsBody implements Threat {
+    TurretAimComponent aimer;
 
     public Turret(float x, float y, float width, float height, World world) {
         super(x, y, width, height, world);
+
+        aimer = new TurretAimComponent(this);
     }
 
     @Override
@@ -23,11 +27,16 @@ public class Turret extends PhysicsBody implements Threat {
 
     @Override
     public void init() {
-
+        aimer.init();
     }
 
     @Override
     public void touchPlayer(Player player) {
 
+    }
+
+    @Override
+    public void act(float delta){
+        super.act(delta);
     }
 }
