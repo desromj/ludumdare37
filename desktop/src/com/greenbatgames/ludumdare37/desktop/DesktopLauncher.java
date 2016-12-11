23a93e96546bfd1dd90f4758f05df5f051f33502
Mike <desrochers.m.j@gmail.com>
2016-12-11 13:53:23 -0700

    Add background image and slightly edit resolution

--- desktop/src/com/greenbatgames/ludumdare37/desktop/DesktopLauncher.java
+++ desktop/src/com/greenbatgames/ludumdare37/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-        config.width = 1200;
+        config.width = 1125;
         config.height = 900;
 
 		new LwjglApplication(new DareGame(), config);
